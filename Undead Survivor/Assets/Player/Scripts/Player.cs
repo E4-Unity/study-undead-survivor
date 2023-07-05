8b14af80f10402c554e07f92d124fab605a15aa6
Eu4ng <dnjsvltm911@gmail.com>
2023-07-05 11:06:41 +0900

    셀 애니메이션 및 애니메이터 제작 및 할당 - 애니메이터 오버리이드 컨트롤러 사용

--- Undead Survivor/Assets/Player/Scripts/Player.cs
+++ Undead Survivor/Assets/Player/Scripts/Player.cs
@@ -12,6 +12,8 @@ public class Player : MonoBehaviour
 
     // 컴포넌트
     Rigidbody2D m_Rigidbody;
+    SpriteRenderer m_Renderer;
+    Animator m_Animator;
     
     // 버퍼
     Vector2 m_InputValue;
@@ -26,11 +28,23 @@ public class Player : MonoBehaviour
     void Awake()
     {
         m_Rigidbody = GetComponent<Rigidbody2D>();
+        m_Renderer = GetComponent<SpriteRenderer>();
+        m_Animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
         // 움직임
         m_Rigidbody.MovePosition(m_Rigidbody.position + m_Speed * Time.fixedDeltaTime * m_InputValue.normalized);
+    }
+
+    void LateUpdate()
+    {
+        // 애니메이션 변수 업데이트
+        m_Animator.SetFloat("Speed", m_InputValue.magnitude);
+        
+        // 움직임에 따라 바라보는 방향 전환
+        if (m_InputValue.x == 0) return;
+        m_Renderer.flipX = m_InputValue.x < 0;
     }
 }
