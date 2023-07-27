7872c531ca37c3a346fef3b2c2b96f0daa1b1498
Eu4ng <dnjsvltm911@gmail.com>
2023-07-27 16:25:32 +0900

    업적 달성 알림창 제작

--- Undead Survivor/Assets/Scripts/AchieveManager.cs
+++ Undead Survivor/Assets/Scripts/AchieveManager.cs
@@ -11,11 +11,16 @@ public class AchieveManager : MonoBehaviour
         UnlockBean
     }
     
+    /* 필드 */
     [Header("[Reference]")]
     [SerializeField] GameObject[] m_LockCharacters;
     [SerializeField] GameObject[] m_UnlockCharacters;
-
+    [SerializeField] GameObject m_Notice_UI;
+    
     Achieve[] m_Achieves;
+    
+    /* 버퍼 */
+    WaitForSecondsRealtime waitForSecondsRealtime;
 
     /* 메서드 */
     void Init()
@@ -59,13 +64,31 @@ public class AchieveManager : MonoBehaviour
         if (isAchieve && PlayerPrefs.GetInt(_achieve.ToString()) == 0)
         {
             PlayerPrefs.SetInt(_achieve.ToString(), 1);
+
+            for (int i = 0; i < m_Notice_UI.transform.childCount; i++)
+            {
+                bool isActive = i == (int)_achieve;
+                m_Notice_UI.transform.GetChild(i).gameObject.SetActive(isActive);
+            }
+            StartCoroutine(NoticeRoutine());
         }
+    }
+
+    IEnumerator NoticeRoutine()
+    {
+        m_Notice_UI.SetActive(true);
+        
+        yield return waitForSecondsRealtime;
+        
+        m_Notice_UI.SetActive(false);
     }
     
     /* MonoBehaviour */
     void Awake()
     {
         m_Achieves = (Achieve[])Enum.GetValues(typeof(Achieve));
+
+        waitForSecondsRealtime = new WaitForSecondsRealtime(5);
         
         if (!PlayerPrefs.HasKey("MyData"))
             Init();
